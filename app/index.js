f0275c9c936f7756009b03a5ea4f07be584e4234
Max Hallinan <maxhallinan@riseup.net>
2017-09-27 15:58:29 +0200

    Adjust formatting

--- app/index.js
+++ app/index.js
@@ -6,84 +6,84 @@ const _s = require('underscore.string');
 const utils = require('./utils');
 
 module.exports = class extends Generator {
-	constructor(a, b) {
-		super(a, b);
+  constructor(a, b) {
+    super(a, b);
 
-		this.option('org', {
-			type: 'string',
-			desc: 'Publish to a GitHub organization account'
-		});
-	}
-	init() {
-		return this.prompt([{
-			name: 'moduleName',
-			message: 'What do you want to name your module?',
-			default: _s.slugify(this.appname),
-			filter: x => utils.slugifyPackageName(x)
-		}, {
-			name: 'moduleDescription',
-			message: 'What is your module description?',
-			default: ``
-		}, {
-			name: 'githubUsername',
-			message: 'What is your GitHub username?',
-			store: true,
-			validate: x => x.length > 0 ? true : 'You have to provide a username',
-			when: () => !this.options.org
-		}, {
-			name: 'website',
-			message: 'What is the URL of your website?',
-			store: true,
-			validate: x => x.length > 0 ? true : 'You have to provide a website URL',
-			filter: x => normalizeUrl(x)
-		}]).then(props => {
-			const repoName = utils.repoName(props.moduleName);
+    this.option('org', {
+      type: 'string',
+      desc: 'Publish to a GitHub organization account'
+    });
+  }
+  init() {
+    return this.prompt([{
+      name: 'moduleName',
+      message: 'What do you want to name your module?',
+      default: _s.slugify(this.appname),
+      filter: x => utils.slugifyPackageName(x)
+    }, {
+      name: 'moduleDescription',
+      message: 'What is your module description?',
+      default: ``
+    }, {
+      name: 'githubUsername',
+      message: 'What is your GitHub username?',
+      store: true,
+      validate: x => x.length > 0 ? true : 'You have to provide a username',
+      when: () => !this.options.org
+    }, {
+      name: 'website',
+      message: 'What is the URL of your website?',
+      store: true,
+      validate: x => x.length > 0 ? true : 'You have to provide a website URL',
+      filter: x => normalizeUrl(x)
+    }]).then(props => {
+      const repoName = utils.repoName(props.moduleName);
 
-			const tpl = {
-				moduleName: props.moduleName,
-				moduleDescription: props.moduleDescription,
-				camelModuleName: _s.camelize(repoName),
-				githubUsername: this.options.org || props.githubUsername,
-				repoName,
-				name: this.user.git.name(),
-				email: this.user.git.email(),
-				website: props.website,
-				humanizedWebsite: humanizeUrl(props.website),
-			};
+      const tpl = {
+        moduleName: props.moduleName,
+        moduleDescription: props.moduleDescription,
+        camelModuleName: _s.camelize(repoName),
+        githubUsername: this.options.org || props.githubUsername,
+        repoName,
+        name: this.user.git.name(),
+        email: this.user.git.email(),
+        website: props.website,
+        humanizedWebsite: humanizeUrl(props.website),
+      };
 
-			const mv = (from, to) => {
-				this.fs.move(this.destinationPath(from), this.destinationPath(to));
-			};
+      const mv = (from, to) => {
+        this.fs.move(this.destinationPath(from), this.destinationPath(to));
+      };
 
-			this.fs.copyTpl([
-				`${this.templatePath()}/**`,
-			], this.destinationPath(), tpl);
+      this.fs.copyTpl([
+        `${this.templatePath()}/**`,
+      ], this.destinationPath(), tpl);
 
-			mv('babelrc', '.babelrc');
-			mv('editorconfig', '.editorconfig');
-			mv('eslintrc.json', '.eslintrc.json');
-			mv('flowconfig', '.flowconfig');
-			mv('gitattributes', '.gitattributes');
-			mv('gitignore', '.gitignore');
-			mv('travis.yml', '.travis.yml');
-			mv('npmrc', '.npmrc');
-			mv('npmignore', '.npmignore');
-			mv('_package.json', 'package.json');
-		});
-	}
-	_private_method_git() {
-		this.spawnCommandSync('git', ['init']);
-		this.spawnCommandSync('git', ['add', '--all']);
-		this.spawnCommandSync('git', ['commit', '-m', 'Initial commit']);
-	}
-	install() {
-		this.installDependencies({
-			bower: false,
-			npm: false,
-			yarn: true,
-		});
-	}
-	end() {
-		this._private_method_git();
-	}
+      mv('babelrc', '.babelrc');
+      mv('editorconfig', '.editorconfig');
+      mv('eslintrc.json', '.eslintrc.json');
+      mv('flowconfig', '.flowconfig');
+      mv('gitattributes', '.gitattributes');
+      mv('gitignore', '.gitignore');
+      mv('travis.yml', '.travis.yml');
+      mv('npmrc', '.npmrc');
+      mv('npmignore', '.npmignore');
+      mv('_package.json', 'package.json');
+    });
+  }
+  _private_method_git() {
+    this.spawnCommandSync('git', ['init']);
+    this.spawnCommandSync('git', ['add', '--all']);
+    this.spawnCommandSync('git', ['commit', '-m', 'Initial commit']);
+  }
+  install() {
+    this.installDependencies({
+      bower: false,
+      npm: false,
+      yarn: true,
+    });
+  }
+  end() {
+    this._private_method_git();
+  }
 };
--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 export default (str) => {
-	if (typeof str !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof str}`);
-	}
+  if (typeof str !== 'string') {
+    throw new TypeError(`Expected a string, got ${typeof str}`);
+  }
 
-	return str;
+  return str;
 };
